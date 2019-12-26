8b14ac1254f48b00476ce5f9582f43235aef7c5b
denis <denisn2000@gmail.com>
2019-12-26 12:29:42 +0300

    edited all

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const PORT = 4312;
+
 require('http')
     .Server((req, res) => {
         if (req.url === '/'){
             res.end('Hello')}
     })
-    .listen(4312);
+    .listen(process.env.PORT || PORT, () => console.log(process.pid));
